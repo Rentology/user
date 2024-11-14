a2559190ac1dbcf498247b9f1ee7d85933401ade
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-11-14 23:14:11 +0300

    Сделал все поля, кроме id и email необязательными

--- migrations/001_create_users_table.up.sql
+++ migrations/001_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
-                       id BIGSERIAL PRIMARY KEY,
+                       id BIGINT PRIMARY KEY NOT NULL ,
                        email VARCHAR(255) UNIQUE NOT NULL,
-                       name VARCHAR(100) NOT NULL,
+                       name VARCHAR(100),
                        last_name VARCHAR(100),
                        second_name VARCHAR(100),
                        birth_date DATE,
